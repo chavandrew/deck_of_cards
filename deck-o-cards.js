19d6465a4d0ffa56b494debd6c9a4a6e17b7cabd
Andrew Chavez <chavandrew@gmail.com>
2017-09-21 08:51:39 -0500

    deck hw

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,5 +1,3 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
-// Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
 function deck_o_cards() {
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
   var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
@@ -7,27 +5,19 @@ function deck_o_cards() {
   var cards = []; // deck
   var shuffledCards = []; // deck shuffled
 
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
+  for (var i = 0; i < suits.length; i++) {
 
+    for (var j = 0; j < values.length; j++) {
+        cards.push(values[j] + " " + 'of '+ suits[i]);
 
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
+      }
+  }
 
-
-  // Pull the top card from the newly shuffledCards
-
-
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
-
+  console.log(cards)
 }
 
+deck_o_cards();
 
-
-// Fisher-Yates Shuffle
-// http://stackoverflow.com/a/6274398
 function shuffle(array) {
     var counter = array.length, temp, index;
 
